6cdaed5917bf51e84ba59ec1943848460b2b7bf2
IntGrah <intgrah@gmail.com>
2022-03-04 16:31:44 +0000

    Inline utility functions

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -2,7 +2,7 @@
 
 #include "bitboard.h"
 
-void print(const U64 bb) // A debug function which pretty prints a bitboard
+inline void print(const U64 bb) // A debug function which pretty prints a bitboard
 {
   U64 comparator = 1ULL << 63;
   int line = 0;
@@ -37,13 +37,13 @@ const int index[64] = {
  *         Harald Prokop
  *         Keith H. Randall
  */
-int bitScanForward(U64 bb)
+inline int bitScanForward(U64 bb)
 {
   constexpr U64 deBruijn64 = 0x03f79d71b4cb0a89;
   return index[((bb & -bb) * deBruijn64) >> 58];
 }
 
-int magnitude(U64 bb)
+inline int magnitude(U64 bb)
 {
   int index = bitScanForward(bb);
   int file = index % 8;
